fc4cd52a2ce73466beee48e8f9a16a93b3bf3ce7
Alex Whitaker <whitaker.alexander@gmail.com>
2016-09-22 11:47:47 -0500

    complete problem

--- group_01.js
+++ group_01.js
@@ -4,3 +4,46 @@ var boo = ["Boo", "11435", "54000", 3];
 var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
+
+for (var i = 0; i < employees.length; i++) {
+    console.log(getEmployee(employees[i]));
+}
+
+function getEmployee (employee) {
+  var bonusArray = [];
+  bonusArray[0] = employee[0];
+  var percent = percentage(employee);
+  bonusArray[1] = (percent * 100) + '%';
+  var bonus = employee[2] * percent;
+  bonusArray[2] = parseInt(employee[2]) + bonus;
+  bonusArray[3] = parseInt(bonus);
+  return bonusArray;
+}
+
+function percentage (arr){
+  var rating = arr[3];
+  var salary = arr[2];
+  var id = arr[1];
+  var precent = 0;
+
+  if (rating <= 2) {
+    return percent;
+  } else if (rating === 3) {
+    percent = 0.04
+  } else if (rating === 4) {
+    percent = 0.06;
+  } else if (rating === 5) {
+    percent = .1;
+  }
+
+if (id.length <= 4) {
+    percent += 0.05;
+ }
+if (salary > 65000) {
+  percent -= 0.01;
+}
+if (percent > 0.13) {
+  percent = 0.13;
+}
+return percent;
+}
